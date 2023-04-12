3e05f74c13f3571081a4c02162440e76ff66a276
chaudharynabin6 <chaudharynabin6@gmail.com>
2023-04-12 15:31:45 +0545

    structural concurrency

--- _014_structured_concurrency/src/main/java/org/example/ordered_execution/eg_structured_concurrency.kt
+++ _014_structured_concurrency/src/main/java/org/example/ordered_execution/eg_structured_concurrency.kt
@@ -7,16 +7,20 @@ import kotlin.time.Duration.Companion.milliseconds
 //https://www.youtube.com/watch?v=3SX7u5l3CGA
 fun main() {
 //    outer block is synchronous block so they run in order
+    println("first coroutine")
     runBlocking {
 //        inside this coroutine scope the suspend functions also runs in order
         println("task1 < task2")
         task1()
+        println("intermediate task1 and task2")
         task2()
     }
+    println("second coroutine")
     runBlocking {
 //        inside this coroutine scope the suspend functions also runs in order
         println("task3 < task4")
         task4()
+        println("intermediate task3 and task4")
         task3()
     }
 }
